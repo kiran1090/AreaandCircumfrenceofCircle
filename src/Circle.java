4337c6f15bd2242e75dd3c82c17115370ae6470b
kiran_a <iamkiranashok@gmail.com>
2022-12-19 17:44:18 +0530

    first commit

---
+++ src/Circle.java
@@ -0,0 +1,15 @@
+public class Circle
+{
+    public static void main(String[] args)
+    {
+
+       double radius,Area,circumference ;
+        final double pi=3.14;
+        radius = 1;
+        Area = pi * radius * radius;
+        circumference= 2 * pi * radius;
+        System.out.println("Area is " + radius);
+        System.out.println("Circumference is " + circumference);
+    }
+
+}
